d969d849090ead4a66df7ece2425768bd663b38e
tt1863 <tt1863@gmail.com>
2014-03-11 20:02:35 -0400

    Add thread controller

---
+++ MVC_Forum/Controllers/ThreadController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Forum.DAL;
+using MVC_Forum.Models;
+
+namespace MVC_Forum.Controllers
+{
+    public class ThreadController : Controller
+    {
+        private IThreadRepository threadRepository;
+
+        public ThreadController()
+        {
+            this.threadRepository = new ThreadRepository(new MVC_ForumsContext());
+        }
+
+        public ThreadController(IThreadRepository threadRepository)
+        {
+            this.threadRepository = threadRepository;
+        }
+
+        public ViewResult Index(int forumId)
+        {
+            var threads = threadRepository.GetThreads()
+                .Where(t => t.ForumId == forumId)
+                .OrderByDescending(t => t.DateCreated);
+            return View(threads);
+        }
+	}
+}
